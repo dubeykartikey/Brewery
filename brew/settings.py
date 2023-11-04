e64214b55ce14fc41c7eee1dc1a62fd86042b30d
Kartikey Dubey <dubeykartikey284@gmail.com>
2023-11-05 05:05:31 +0530

    Added hosts

--- brew/settings.py
+++ brew/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ja515)r=r3$3=*-&pcegti6gevx1()t8yc#iep$2u-c&g#y4+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "brewery.onrender.com"
+]
 
 
 # Application definition
